4677657f3b51d16d05008067436937217e3d4d6d
Sean Minnick <sean.minnick11@gmail.com>
2025-06-26 14:31:54 -0400

    fixed more quote issues

--- Deployment/deployment1.ps1
+++ Deployment/deployment1.ps1
@@ -58,7 +58,7 @@ try {
 
     Write-Host " Honeypot deployment completed successfully."
 } catch {
-    Write-Error ' Error during honeypot deployment: $($_)'
+    Write-Error " Error during honeypot deployment: $($_)"
 } finally {
     Write-Host 'Deployment Success'
 }
